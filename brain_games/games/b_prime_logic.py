eb48b676372bbb50ea0c0529e3395da0287d3feb
Daniel Kudinov <nippefour@gmail.com>
2025-10-03 17:18:03 +0400

    create fifth game (prime or not)

---
+++ brain_games/games/b_prime_logic.py
@@ -0,0 +1,29 @@
+from random import randint
+
+import brain_games.engine as ngin
+
+import prompt
+
+
+def b_prime():
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
+    while ngin.counter < 3:
+        num = randint(1, 100)
+        div_counter = 0
+        possible_divs = range(1, num // 2)
+        for i in possible_divs:
+            if num % i == 0:
+                div_counter += 1
+        real_answer = 'yes'
+        if div_counter >= 2:
+            real_answer = 'no'
+        print(f'Question: {num}')
+        answer = prompt.string('Your answer: ')
+        if answer == real_answer:
+            print('Correct!')
+            ngin.counter += 1
+            num = randint(1, 100)
+            div_counter = 0 
+        else:
+            print(f"{answer} is wrong answer ;(. Correct answer was {real_answer}. Let's try again, {ngin.name}")
+            quit()
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,11 @@
+from brain_games.engine import run
+
+from brain_games.games.b_prime_logic import b_prime
+
+
+def main():
+    run(b_prime)
+
+
+if __name__ == "__main__":
+    main()
